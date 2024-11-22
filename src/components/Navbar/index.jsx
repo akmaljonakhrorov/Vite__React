b35172fe45db64ccc28cef611719a7f8f368772a
akmaljonakhrorov <akmaljon.akhrorov@mail.ru>
2024-11-22 10:57:02 +0500

    import links from react icons deleted

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import { Container, Logo, Section } from "./index";
 import logoImg from "../../assets/images/logoImg.png";
 import Navlinks from "../Navlinks/index.jsx";
-// icons
-// import { FaTelegram } from "react-icons/fa6";
-// import { FaInstagramSquare, FaFacebook } from "react-icons/fa";
-
 const Navbar = () => {
   return (
     <Container>
@@ -17,13 +13,6 @@ const Navbar = () => {
           <Navlinks />
         </ul>
       </Section>
-      {/* <Section> */}
-      {/* <div className="contact">
-          <FaTelegram className="nav-icon" />
-          <FaInstagramSquare className="nav-icon" />
-          <FaFacebook className="nav-icon" />
-        </div> */}
-      {/* </Section> */}
     </Container>
   );
 };
